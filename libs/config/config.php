39412d9ffda002d0a3e7e5d4f6dc242726e44e79
Mama679 <ingmariomanotas@hotmail.com>
2021-12-03 14:28:11 -0500

    Archivos configuracion de Base de datos

---
+++ libs/config/config.php
@@ -0,0 +1,9 @@
+<?php
+define('URL','http://localhost:85/expenseApp');
+
+define('HOST','localhost');
+define('DB','expenceapp');
+define('USER','root');
+define('PASSWORD','');
+define('CHARSET','utf-8');
+?>
